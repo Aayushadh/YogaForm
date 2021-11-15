177eed33f1f72a31532ab1c9bb8f24bd084064c0
Aayush Harwani <ayushharwani2011@gmail.com>
2021-11-15 12:44:45 +0530

    init

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,6 @@ app.use((err, req, res, next) => {
 })
 
 const __dirname = path.resolve()
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
